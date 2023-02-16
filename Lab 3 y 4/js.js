cb0989d97c6e0420a4f6afe6c7c4d7b0fd9baa75
Alejandra Cabrera <alejandraa.cruiz@gmail.com>
2023-02-16 12:07:42 -0600

    Agregar carpeta de trabajo para lab 3 y 4

---
+++ Lab 3 y 4/js.js
@@ -0,0 +1,6 @@
+//variables constantes, consola (log, info, warn, error, assert)
+//Alcance de las variables
+// se usa var y let para deinir variables
+//se usa const para que el cliente no modifique el valor 
+//console.log("") registro en la consola
+//console.info("") mostrar una información, concatenar con + 
