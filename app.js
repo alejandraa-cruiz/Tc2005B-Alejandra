e8946efb3daa64b6998d13cf9a81f013d2ad376d
Alejandra Cabrera <alejandraa.cruiz@gmail.com>
2023-03-28 11:44:39 -0600

    Starting Lab 20, Lab 21 sql and Lab 22 sql

--- app.js
+++ app.js
@@ -5,9 +5,34 @@ const session = require('express-session');
 const isAuth = require('./util/is-auth');
 
 const app = express();
-
+const multer = require('multer');
 
 app.use(bodyParser.urlencoded({extended: false}));
+
+
+
+//fileStorage: Es nuestra constante de configuración para manejar el almacenamiento
+const fileStorage = multer.diskStorage({
+    destination: (request, file, callback) => {
+        //'uploads': Es el directorio del servidor donde se subirán los archivos 
+        callback(null, 'uploads');
+    },
+    filename: (request, file, callback) => {
+        //aquí configuramos el nombre que queremos que tenga el archivo en el servidor, 
+        //para que no haya problema si se suben 2 archivos con el mismo nombre concatenamos el timestamp
+        callback(null, new Date().toISOString() + '-' + file.originalname);
+    },
+});
+
+//Idealmente registramos multer después de bodyParser (la siguiente línea ya debería existir)
+app.use(bodyParser.urlencoded({ extended: false }));
+
+//En el registro, pasamos la constante de configuración y
+//usamos single porque es un sólo archivo el que vamos a subir, 
+//pero hay diferentes opciones si se quieren subir varios archivos. 
+//'archivo' es el nombre del input tipo file de la forma
+app.use(multer({ storage: fileStorage }).single('archivo'));
+
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
